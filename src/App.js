fc82168550ce47cbc1f8b2c5c36950e7aa223572
allen4dev <alanaliagadev@gmail.com>
2017-10-25 00:37:00 -0400

    [Add] base structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
+import { Provider } from 'react-redux';
+
+import store from './store';
+
+const App = () => {
+  return (
+    <Provider store={store}>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hey</h1>
       </div>
-    );
-  }
-}
+    </Provider>
+  );
+};
 
 export default App;
